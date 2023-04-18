f26af85763801d9702ad96626eaeeb5e561001b7
Rubi <quipu1@naver.com>
2023-04-18 20:07:22 +0900

    [BOJ] 11382 / 꼬마 정민 / 브론즈5

---
+++ quipu1/단계별/입출력과 사칙연산/[BOJ]11382.java
@@ -0,0 +1,25 @@
+package basic;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        bw.write(String.valueOf(a+b+c));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
