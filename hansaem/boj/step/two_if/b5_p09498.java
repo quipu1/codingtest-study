9f850dc2ed7127545b41963c5cfdb7512fa8489e
hxnsxxm <hansaemj20@gmail.com>
2023-04-18 22:42:35 +0900

    [BOJ] 9498 / 시험 성적 / 브론즈5

---
+++ hansaem/boj/step/two_if/b5_p09498.java
@@ -0,0 +1,25 @@
+package boj.step.two_if;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class b5_p09498 {
+	public static void main(String[] args) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(
+				new FileReader("hansaem/boj/step/two_if/input/p09498.txt")
+		);
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int score = Integer.parseInt(st.nextToken());
+		
+		if (score >= 90) System.out.println("A");
+		else if (score >= 80) System.out.println("B");
+		else if (score >= 70) System.out.println("C");
+		else if (score >= 60) System.out.println("D");
+		else System.out.println("F");
+	}
+}
