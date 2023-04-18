ef62b54ca510d68fa07d4df1170c7374e0cead56
Rubi <quipu1@naver.com>
2023-04-18 19:49:29 +0900

    [BOJ] 10430 / 나머지 / 브론즈5

---
+++ quipu1/단계별/입출력과 사칙연산/[BOJ]10430.java
@@ -0,0 +1,31 @@
+package basic;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf((a+b)%c));
+        bw.newLine();
+        bw.write(String.valueOf(((a%c) + (b%c))%c));
+        bw.newLine();
+        bw.write(String.valueOf((a*b)%c));
+        bw.newLine();
+        bw.write(String.valueOf(((a%c)*(b%c))%c));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
