1884cb097565d26e45f7655d4073b9a72bc55a9c
Rubi <quipu1@naver.com>
2023-04-18 20:59:43 +0900

    [BOJ] 2753 / 윤년 / 브론즈5

---
+++ quipu1/단계별/조건문/[BOJ]2753.java
@@ -0,0 +1,27 @@
+package basic;
+
+import java.io.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int year = Integer.parseInt(br.readLine());
+
+        if (year % 4 == 0 && year % 100 != 0 ) {
+            bw.write("1");
+        } else if (year % 400 == 0) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
