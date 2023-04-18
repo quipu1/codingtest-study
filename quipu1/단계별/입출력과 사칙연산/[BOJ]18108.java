dcf52721a76b59eaff22530dba42e61347ee4810
Rubi <quipu1@naver.com>
2023-04-18 19:44:36 +0900

    [BOJ] 18108 / 1998년생인 내가 태국에서는 2541년생?! / 브론즈5

---
+++ quipu1/단계별/입출력과 사칙연산/[BOJ]18108.java
@@ -0,0 +1,21 @@
+package basic;
+
+import java.io.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int y = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(y-543));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
