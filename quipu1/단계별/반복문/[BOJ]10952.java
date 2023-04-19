bcb5e680f111ca4132b221b823cebd74f652ebb5
Rubi <quipu1@naver.com>
2023-04-19 21:30:03 +0900

    [BOJ] 2439 / A+B-5 / 브론즈5

---
+++ quipu1/단계별/반복문/[BOJ]10952.java
@@ -0,0 +1,32 @@
+package basic;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+
+            bw.write(String.valueOf(a+b));
+            bw.newLine();
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
