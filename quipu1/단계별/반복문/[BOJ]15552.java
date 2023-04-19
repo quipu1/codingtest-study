7a9cb708a823fbfc3edc47f5ccff4029f4127f5d
Rubi <quipu1@naver.com>
2023-04-19 21:13:21 +0900

    [BOJ] 15552 / 빠른 A+B / 브론즈4

---
+++ quipu1/단계별/반복문/[BOJ]15552.java
@@ -0,0 +1,29 @@
+package basic;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int i=0; i < tc; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(a+b));
+            bw.newLine();
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
