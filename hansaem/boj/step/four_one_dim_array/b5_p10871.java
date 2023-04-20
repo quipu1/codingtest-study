be47199bf113e11675cae5f83a71ebfddbaa32ad
hxnsxxm <hansaemj20@gmail.com>
2023-04-20 11:46:12 +0900

    [BOJ] 10871 / X보다 작은 수 / 브론즈 5

---
+++ hansaem/boj/step/four_one_dim_array/b5_p10871.java
@@ -0,0 +1,33 @@
+package boj.step.four_one_dim_array;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class b5_p10871 {
+	public static void main(String[] args) throws IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(
+				new FileReader("hansaem/boj/step/four_one_dim_array/input/p10871.txt")
+				);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		
+		int answer = 0;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int tmp = Integer.parseInt(st.nextToken());
+			if (X < tmp) sb.append(tmp).append(" ");
+		}
+		bw.write(sb.toString());
+		bw.close();
+	}
+}
