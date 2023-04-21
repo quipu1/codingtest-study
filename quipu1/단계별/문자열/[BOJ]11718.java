7f96af331cfe740ad06601784d50c0ebcf900dcd
Rubi <quipu1@naver.com>
2023-04-21 19:41:54 +0900

    [BOJ] 11718 / 그대로 출력하기 / 브론즈5

---
+++ quipu1/단계별/문자열/[BOJ]11718.java
@@ -0,0 +1,24 @@
+package basic;
+
+import java.io.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = "";
+
+        while ((input = br.readLine()) != null && !input.isEmpty()) {
+            bw.write(input);
+            bw.newLine();
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
