5f52d597b8bf476cb97914838710196cda83c5aa
Rubi <quipu1@naver.com>
2023-04-21 18:24:24 +0900

    [BOJ] 2743 / 단어 길이 재기 / 브론즈5

---
+++ quipu1/단계별/문자열/[BOJ]2743.java
@@ -0,0 +1,21 @@
+package basic;
+
+import java.io.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+
+        bw.write(String.valueOf(s.length()));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
