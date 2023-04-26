61c4001df02d0d6160940cfce585f9acf2b78028
Rubi <quipu1@naver.com>
2023-04-26 19:41:55 +0900

    [BOJ] 2292 / 벌집 / 브론즈2

---
+++ quipu1/단계별/일반 수학/2292.java
@@ -0,0 +1,28 @@
+package basic;
+
+import java.io.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int move = 1;
+        int max = 1;
+        while (n>max) {
+            max += 6*move;
+            move += 1;
+        }
+
+        bw.write(String.valueOf(move));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
