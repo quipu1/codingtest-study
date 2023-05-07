76f3402ccff056038236471214cf68b324492e6e
hxnsxxm <hansaemj20@gmail.com>
2023-05-07 20:02:15 +0900

    [BOJ] 1436 / 영화감독 숌 / 실버 5

--- hansaem/백준/단계별/브루트포스/s5_p01436.java
+++ hansaem/백준/단계별/브루트포스/s5_p01436.java
@@ -1,13 +1,15 @@
 package 백준.단계별.브루트포스;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class s5_p01436 {
     public static void main(String[] args) throws IOException {
         //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader br = new BufferedReader(
-                new FileReader("hansaem/백준/단계별/브루트포스/input/p01436.txt")
+                new FileReader("hansaem/백준/단계별/브루트포스/input/p02798.txt")
         );
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -17,20 +19,13 @@ public class s5_p01436 {
         int N = Integer.parseInt(st.nextToken());
 
         int count = 0;
-        if (N % 5 == 0) sb.append(N / 5);
-        else {
-            int maxFive = N / 5;
-            for (int i = maxFive; i >= 0; i--) {
-                int tmp = N - 5 * i;
-                if (tmp % 3 == 0) {
-                    count = i + tmp / 3;
-                    break;
-                }
-                //System.out.println(i + ", " + tmp + ", "+ count);
-            }
-            if (count == 0) count = -1;
-            sb.append(count);
+        int idx = 0;
+        while (count < N) {
+            idx += 1;
+            if (String.valueOf(idx).contains("666")) count += 1;
+            if (count == N) break;
         }
+        sb.append(idx);
 
         bw.write(sb.toString());
         bw.flush();
